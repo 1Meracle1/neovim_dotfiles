90e2bb7e308c93cc8b2841a5687a8d97d714cc4f
1Meracle1 <bahorin.ivan@gmail.com>
2023-06-09 08:39:23 +0200

    change in autopairs config

--- init.lua
+++ init.lua
@@ -546,7 +546,41 @@ require("tokyonight").setup({
   on_highlights = function(highlights, colors) end,
 })
 
-require 'autopairs-config'
+-- Setup autopairs with nvim-cmp.
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
+
+
+npairs.setup {
+  check_ts = true,
+  ts_config = {
+    lua = { "string", "source" },
+    javascript = { "string", "template_string" },
+    java = false,
+  },
+  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    offset = 0, -- Offset from pattern match
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "PmenuSel",
+    highlight_grey = "LineNr",
+  },
+}
+
+
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
 vim.cmd [[colorscheme tokyonight-night]]
 -- The line beneath this is called `modeline`. See `:help modeline`

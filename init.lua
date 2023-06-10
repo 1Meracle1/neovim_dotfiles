cff6fa25c235767730186e9f4abf139657282544
1Meracle1 <bahorin.ivan@gmail.com>
2023-06-10 17:24:20 +0200

    updated

--- init.lua
+++ init.lua
@@ -398,6 +398,9 @@ local on_attach = function(_, bufnr)
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
+  nnoremap("<C-d>", "<C-d>zz")
+  nnoremap("<C-u>", "<C-u>zz")
+
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
@@ -510,41 +513,6 @@ cmp.setup {
   },
 }
 
-require("tokyonight").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "night",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  light_style = "night",  -- The theme is used when the background is set to light
-  transparent = false,    -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark",              -- style for sidebars, see below
-    floats = "dark",                -- style for floating windows
-  },
-  sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false,             -- dims inactive windows
-  lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-})
 
 -- Setup autopairs with nvim-cmp.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
@@ -582,6 +550,27 @@ if not cmp_status_ok then
 end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
-vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd [[colorscheme habamax]]
+vim.cmd [[echo expand('%:p')]]
+
+function _G.current_tab()
+  local curr_buf = vim.fn.bufnr()
+  local total = 0
+  local curr_tab
+
+  for i = 1, vim.fn.tabpagenr('$') do
+    total = total + 1
+    for _, bufnr in ipairs(vim.fn.tabpagebuflist(i)) do
+      if bufnr == curr_buf then
+        curr_tab = i
+      end
+    end
+  end
+
+  return string.format('(%d of %d)', curr_tab, total)
+end
+
+-- vim.opt.titlestring = [[%f %h%m%r%w - %{v:progname} %{luaeval('current_tab()')}]]
+-- vim.o.titlestring = [[%{expand('%:p')}]]
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
